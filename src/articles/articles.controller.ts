88af6df436a9f89202f0e69476dac6b77f428ed8
Mauro <mneagoe@agn.gov.ar>
2023-08-09 17:58:45 -0300

    completed: Group endpoints together in Swagger

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('articles')
+@ApiTags('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articlesService.create(createArticleDto);
+  }
+
+  @Get('drafts')
+  findDrafts() {
+    return this.articlesService.findDrafts();
   }
 
   @Get()
